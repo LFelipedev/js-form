7bc76bbd2f5d09b3b2f01eab85ae9a6e44addd38
LFelipedev <luiz.fma.up@gmail.com>
2025-08-13 16:55:04 -0300

    corrigindo o problema de poder enviar o formulário vazio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const form = document.querySelector(".form-container");
 const formSubmit = document.getElementById("btn-submit");
 
 formSubmit.addEventListener("click", function(event) {
-event.preventDefault(); 
-alert("Sua denuncia foi realizada com sucesso!");
+    if (form.checkValidity()) {
+        alert("Sua denuncia foi realizada com sucesso!");
+    } else {
+        alert("É necessário que todos os campos sejam preenchidos")
+    }
 });
