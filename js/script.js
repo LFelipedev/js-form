6be9dd29989087f9f3610ffb55c5682a305cc9a0
LFelipedev <luiz.fma.up@gmail.com>
2025-08-13 12:55:27 -0300

    form assedio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const formSubmit = document.getElementById("btn-submit");
+
+formSubmit.addEventListener("click", function(event) {
+event.preventDefault(); 
+alert("Sua denuncia foi realizada com sucesso!");
+});
